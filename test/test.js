fba06b0cd84bd22789ee934d8e6efa4b494e4115
kenny335588 <kenny335588@gmail.com>
2017-12-01 11:17:11 +0800

    add lib

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect
 
 describe('morse', function () {
-  var jon = require('../lib/morse')
+  var morse = require('./lib/morse')
 
   describe('字母轉摩斯電碼測試', function () {
     it('morse.convert(寫一個函數將該字串轉為摩斯電碼)', function () {
@@ -9,7 +9,7 @@ describe('morse', function () {
     })
   })
   describe('摩斯電碼轉字母測試', function () {
-    it('morse.iconvert(写一个函数将该字串转为简体)', function () {
+    it('morse.iconvert()', function () {
       expect(morse.iconvert('')).to.equal('寫一個函數將該摩斯電碼轉為字串')
     })
   })
